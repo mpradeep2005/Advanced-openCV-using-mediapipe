e0387e3cd9bb388219a29530cff96d95c77b760d
Admin <pradeep14.3.2005@gmail.com>
2025-09-07 10:31:48 +0530

    fps counter and single point detection

--- Hand_Recognizer_min.py
+++ Hand_Recognizer_min.py
@@ -1,10 +1,14 @@
 import cv2 as cv
 import mediapipe as mp
+import time
 
 cap=cv.VideoCapture(0)
 mp_hand=mp.solutions.hands
 mp_draw=mp.solutions.drawing_utils
 hands=mp_hand.Hands()
+
+ptime=0
+ctime=0
 
 while True:
     isTrue,frame=cap.read()
@@ -13,10 +17,25 @@ while True:
 
     if result.multi_hand_landmarks:
         for hand in result.multi_hand_landmarks:
+            for id,lm in enumerate(hand.landmark):
+                h,w,c=img.shape
+                cx,cy=int(lm.x*w),int(lm.y*h)
+                if id==8:
+                    cv.circle(frame,(cx,cy),15,(200,0,0),cv.FILLED)
             mp_draw.draw_landmarks(frame,hand,mp_hand.HAND_CONNECTIONS)
-
-    cv.imshow("hand",cv.flip(frame,1))
-    if cv.waitKey(10) & 0xFF==ord('q'):
+    ctime=time.time()
+    fps=1/(ctime-ptime)
+    ptime=ctime
+    frame=cv.flip(frame,1)
+    cv.putText(frame,
+               str(int(fps)),
+               (10, 70),
+               cv.FONT_HERSHEY_SIMPLEX,
+               3,
+               (200,0, 90),
+               3)
+    cv.imshow("hand",frame)
+    if cv.waitKey(10) & 0xFF==27:
         break
 
 cap.release()
